0f62217c6fad47428cb57147167155a46fc255cd
Malin S <malin.soderstrom@iths.se>
2021-05-27 15:13:09 +0200

    testing new navigation

--- src/screens/home.js
+++ src/screens/home.js
@@ -2,13 +2,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Home() {
+export default function Home(props) {
+
+const handlePress = () => {
+    props.navigation.navigate('Login')
+}    
+
   return (
     <View style={styles.container}>
       <Text>Homepage!</Text>
+      <Button
+        title="Login"
+        onPress={() => handlePress}
+        />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,11 +1,20 @@
 //import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function Login() {
+export default function Login(props) {
+
+    const handlePress = () => {
+        props.navigation.navigate('Home')
+    }
+
   return (
     <View>
       <Text>Login!</Text>
+      <Button
+        title="Home"
+        onPress={() => handlePress}
+      />
     </View>
   );
 }
