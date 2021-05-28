d8a61b1de2f81f1149030b4001b5746402026849
Malin S <malin.soderstrom@iths.se>
2021-05-28 14:27:28 +0200

    basic styling in menu, added bg-image in assets

--- src/screens/menu.js
+++ src/screens/menu.js
@@ -1,39 +1,79 @@
 //import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Pressable } from 'react-native';
 
-export default function Menu(props) {    
+export default function Menu(props) {
+
+    const image = require('../assets/bg_menu.jpg')
 
     const handlePress = (page) => {
         props.navigation.navigate(page)
     }
 
 
-  return (
-    <View style={styles.container}>
-      <Text>Menu!</Text>
-      <Text
-        onPress={() => handlePress('Home')}
-      >Home</Text>
-      <Text
-        onPress={() => handlePress('Tasks')}
-      >My tasks</Text>
-      <Text
-        onPress={() => handlePress('Create task')}
-      >Create new task</Text>
-      <Button
-        title="sign out"
-      />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={image} style={styles.image}>
+                <View style={styles.list}>
+                <Text
+                    style={styles.links}
+                    onPress={() => handlePress('Home')}
+                >Home</Text>
+                <Text
+                    style={styles.links}
+                    onPress={() => handlePress('Tasks')}
+                >My tasks</Text>
+                <Text
+                    style={styles.links}
+                    onPress={() => handlePress('Create task')}
+                >Create new task</Text>
+                </View>
+                <Pressable style={styles.button}> 
+                <Text style={styles.buttonText}>Sign Out</Text> 
+                </Pressable>
+            </ImageBackground>
+        </View>
+    );
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        width: '100%',
+        height: '100%',
+        alignItems: "center",
+        opacity: 0.7
+    },
+    list: {
+        marginTop: -200
+    },
+    links: {
+        textAlign: 'center',
+        fontSize: 30,
+        backgroundColor: '#3B3B3B',
+        color: 'white',
+        marginBottom: 30,
+        padding: 10,
+        paddingHorizontal: 40,
+        borderRadius: 20
+    },
+    buttonText: {
+        fontSize: 30,
+    },
+    button: {
+        backgroundColor: '#4e9ac7',
+        color: 'white',
+        marginTop: 20,
+        padding: 10,
+        paddingHorizontal: 40,
+        borderRadius: 20
+    }
 });
