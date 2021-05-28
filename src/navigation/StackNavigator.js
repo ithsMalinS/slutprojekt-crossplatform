b5110abb1da6d2ccac88af85cfbb8b5bfc7159ef
Malin S <malin.soderstrom@iths.se>
2021-05-28 09:52:54 +0200

    addded screens in stacknavigator

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -3,6 +3,10 @@ import React from 'react';
 
 import Login from '../screens/login'
 import Home from '../screens/home'
+import Menu from '../screens/menu';
+import CreateTask from '../screens/newTask';
+import TaskDetails from '../screens/taskDetails';
+import Tasks from '../screens/tasks';
 
 
 const Stack = createStackNavigator()
@@ -11,6 +15,10 @@ const MainStack = () => {
         <Stack.Navigator initialRouteName="Login">
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Menu" component={Menu} />
+            <Stack.Screen name="Create task" component={CreateTask} />
+            <Stack.Screen name="Task details" component={TaskDetails} />
+            <Stack.Screen name="Tasks" component={Tasks} />
         </Stack.Navigator>
     )
 }
