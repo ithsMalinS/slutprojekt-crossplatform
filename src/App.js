59fbcc82212576729513b40de16ba292991fa835
Viktor Okomdal <viktor_okomdal@hotmail.com>
2021-05-31 13:29:44 +0200

    Drawer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
-import MainStack from './navigation/StackNavigator'
+// import MainStack from './navigation/StackNavigator'
+import Drawer from './navigation/drawer'
+
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MainStack />
+      <Drawer />
     </NavigationContainer>
   );
 }
