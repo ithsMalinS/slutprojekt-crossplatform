d1caf906641eebd1e708f56c4692e5346ec43a26
Malin S <malin.soderstrom@iths.se>
2021-06-09 18:57:00 +0200

    removed old code, added heading and menu icon with toggle. basic styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,25 @@
-import React, {useContext} from 'react';
-import { StyleSheet, Button, View, Text } from 'react-native';
-import {MyContext} from '../storage/context'
+import React from 'react';
+import { StyleSheet, View, Image, TouchableHighlight, Text } from 'react-native';
 
 export default function Header(props) {    
-  const {theme, switchTheme} = useContext(MyContext)
-
-  const changeTheme = () => theme == 'dark' ? switchTheme('light') : switchTheme('dark')
   
   return (
     <View style={styles.container}>
-      <Button
-        title = "Toggle" onPress={() => props.navigation.toggleDrawer()}   
-      />
-      
-      <Button
-        title = "Toggle2" onPress={changeTheme}   
-      />
-      
-      <Text style={styles[theme]}>Dragon Force!</Text>
-      
+      <Text style={styles.heading}>Inge Bra Bygg</Text>
+      <TouchableHighlight 
+        onPress={() => props.navigation.toggleDrawer()}
+        style={styles.touchable}
+        activeOpacity={0.7}
+        underlayColor="#DDDDDD"
+        >
+                
+        <Image 
+          source={require("../assets/menu.png")}
+          style={styles.logo}
+        />
+
+      </TouchableHighlight>
+
     </View>
   );
 }
@@ -28,18 +29,22 @@ const styles = StyleSheet.create({
   container: {  
     width: '100%',
     paddingTop: 40,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    flexDirection:'row'
+    backgroundColor: '#869f9f',
+    flexDirection:'row',
+    justifyContent: 'flex-end'
   },
-  dark: {
-    
-    color: 'blue',
-    backgroundColor: 'black',
+  logo:{
+    width: 30,
+    height: 30,
   },
-  light: {
-   
-    color: 'red',
-    backgroundColor: 'white',
+  touchable:{
+    marginRight: 30,
+    marginTop: 8,
+  },
+  heading: {
+    color: '#4e6262',
+    marginRight: 50,
+    fontSize: 30,
+    marginBottom: 10,
   }
 });
